48a0312e540276a8669a782fd6e40ab41736ba2d
huanghongda <l>
2021-02-05 15:21:06 +0800

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import {Fragment} from 'react'
+// Fragment可理解为vue template
+function App() {
+    return (
+        <Fragment>
+            <div className="App">
+                <h2>这是一个React脚手架</h2>
+            </div>
+        </Fragment>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Descripttion: Damom
+ * @version: v1.0
+ * @Author: hongda_huang
+ * @Date: 2021-02-05 12:43:34
+ * @LastEditTime: 2021-02-05 15:18:26
+ * @description: 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './common/style/index.css'
+import App from './App';
+
+
+ReactDOM.render(
+    <App/>,document.getElementById('root')
+);
+
+
+
